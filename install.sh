1fca4e782061e8fadd3bfd9d6ff4b9a999794f51
Lydia Katsamberis <llkats@gmail.com>
2014-06-14 21:39:15 -0700

    install script fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 dir="$HOME/Developer/llkats"
 mkdir -p $dir
 cd $dir
-git clone git://github.com/paulmillr/dotfiles.git
+git clone git://github.com/llkats/dotfiles.git
 cd dotfiles
 sudo bash symlink-dotfiles.sh
